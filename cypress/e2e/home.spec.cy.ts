2cc772abaa4fadf635a570b85ba79890c3ac74ec
Cláudio Rapôso <engcfraposo@gmail.com>
2022-07-09 10:04:08 -0300

    add_e2e_test

---
+++ cypress/e2e/home.spec.cy.ts
@@ -0,0 +1,21 @@
+describe('Teste de fluxo da pagina de Home', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+  it("Se eu renderizar a pagina de Home e clicar no botão de menos o valor não muda",() => {
+    cy.get('#remove-count').click()
+    cy.get("#count-div").should("contain", "0")
+  })
+  it("Se eu renderizar a pagina de Home e clicar no botão de mais o valor de ser um", () => {
+    cy.get('#add-count').click()
+    cy.get("#count-div").should("contain", "1")
+  })
+  it("Se eu renderizar a pagina de Home e clicar duas vez no botão mais e uma vez no clicar no botão menos o valor deve ser um",()=> {
+    cy.get('#add-count').click()
+    cy.get("#count-div").should("contain", "1")
+    cy.get('#add-count').click()
+    cy.get("#count-div").should("contain", "2")
+    cy.get('#remove-count').click()
+    cy.get("#count-div").should("contain", "1")
+  })
+})
